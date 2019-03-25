44683c5caab4274757dc17530079bcd944edc505
tejvi-m <41098307+tejvi-m@users.noreply.github.com>
2019-03-25 23:06:33 +0530

    Add files via upload

---
+++ Phase-2/UI/process.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  <style type ="text/css">
+  div.s{
+    position:relative;
+    top:50px;
+    height:50px;
+    color:;
+    width:100%;
+    textalign:centre;
+    font-size:30px;
+    padding:17px;
+    padding-left:70px;
+  }
+
+  </style>
+  </head>
+  <body>
+  
+  <div class = "s">your response has been recorded.</div>
+  </body>
+</html>
---
+++ Phase-2/UI/student1.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style type="text/css">
+    div.s{
+      position:relative;
+      top:50px;
+      height:50px;
+      color:;
+      width:100%;
+      textalign:centre;
+      font-size:30px;
+      padding:17px;
+      padding-left:70px;
+    }
+    div.cPanels{
+      position:relative;
+      top:50px;
+      height:50px;
+      color:;
+      width:100%;
+      textalign:centre;
+      font-size:15px;
+      padding:37px;
+      padding-left:50;
+    }
+
+    .student{
+
+        position:relative;
+        top:10px;
+        height:30px;
+        color:;
+        width:100%;
+        textalign:centre;
+        font-size:15px;
+        padding:10px;
+        line-height: 1.6;
+    }
+
+    input[type=text] {
+    padding: 0;
+    height: 30px;
+    position: relative;
+    left: 0;
+    outline: none;
+    border: 1px solid #cdcdcd;
+    border-color: rgba(0,0,0,.15);
+    background-color: white;
+    font-size: 16px;
+}
+.advancedSearchTextbox {
+    width: 526px;
+    margin-right: -4px;
+}
+
+    #save{
+
+    background-color: #067c74;
+    border: none;
+    color: white;
+    padding: 16px 32px;
+    text-decoration: none;
+    margin: 4px 2px;
+    cursor: pointer;
+  }
+  select {
+
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+
+  padding: 12px 16px;
+  z-index: 1;
+}
+
+    </style>
+  </head>
+  <body>
+    <form action ="process.php" method = "post">
+      <div class = cPanels>
+      Select team size:
+      <input type ="radio" name = "size" id="size2" value="2"/>2
+
+      <input type ="radio" name = "size" id="size3" value="3"/>3</br>
+      Project Title: <input type = "text" name = "Ptitle"/></br></br>
+      Domain: <input type = "text" name = "Domain"/></br></br>
+      Guide: <select name="guide">
+
+        <?php
+
+        $user = "";
+        $pswd = "";
+        $db_connection = pg_connect("host=localhost dbname=register user=".$user." password=".$pswd);
+        $result = pg_query($db_connection, "SELECT * FROM teacher;");
+        echo pg_last_error($dbconn);
+        echo $result;
+        while ($row = pg_fetch_row($result)) {
+          echo "<option value=\"$row[1]\">$row[0]</option>";
+          }
+          ?>
+      <br>
+      <br>
+      </select>
+      <div id="fortext"></div>
+    </br></br>
+      <input type="submit" id = "save" name="submit" value="save" />
+      <script>
+      var el1 = document.getElementById("fortext");
+      var newEL = document.createElement("p");
+
+      function addText(x){
+        newEL.innerHTML = "";
+        for(var i = 1; i <= x; i++){
+          newEL.innerHTML += "<div class = student>";
+          newEL.innerHTML += "Teammate " + i + " details</br>";
+          newEL.innerHTML += "Name: <input type =\"text\" name=\"name" + i + "\"\>";
+          newEL.innerHTML += "</br>SRN: <input type=\"text\" name = \"SRN" + i + "\"/></br>";
+          newEL.innerHTML += "CGPA: <input type=\"text\" name = \"CGPA" + i + "\"/></br>";
+          newEL.innerHTML += "</div>";
+        }
+
+        el1.appendChild(newEL);
+
+      }
+
+      function make2(e){
+        console.log("team size is 2");
+        addText(2);
+      };
+
+      function make3(e){
+        console.log("team size is 3");
+        addText(3);
+      };
+
+      var element1 = document.getElementById("size2");
+      element1.addEventListener("click", make2);
+      var element2 = document.getElementById("size3");
+      element2.addEventListener("click", make3);
+
+      </script>
+
+</div>
+  </body>
+</html>
