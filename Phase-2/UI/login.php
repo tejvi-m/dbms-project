ecd369a6471c3a4d10bf8e1803438795e69c53bf
Tejvi M <tejvimstays@gmail.com>
2019-04-19 22:45:31 +0530

    added login page. does not check for password.

---
+++ Phase-2/UI/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form action="loginprocess.php" method = "POST">
+      ID: <input type="text" name = "username"/>
+      <br>
+      Password: <input type="password" name = "password"/>
+      <br>
+      <input type="submit" value ="login"/>
+    </form>
+      </body>
+</html>
